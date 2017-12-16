35ac0c35995ae1b846b1fd97676344a02473f807
Mark Chapman <mark.chapman@gmail.com>
2017-12-16 14:50:50 +0000

    Added a test

--- test/test.js
+++ test/test.js
@@ -2,22 +2,27 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const RRule = require("rrule-alt");
 const chai = require("chai");
-const index_1 = require("../index");
+const rrule_ops_1 = require("../rrule-ops");
 let assert = chai.assert;
 describe('intersections', function () {
     it('handles non intersecting weekly', function () {
-        assert.isNull(index_1.intersection(RRule.fromString('FREQ=WEEKLY;WKST=MO;BYDAY=TU'), RRule.fromString('FREQ=WEEKLY;WKST=MO;BYDAY=WE')));
+        assert.isNull(rrule_ops_1.intersection(RRule.fromString('FREQ=WEEKLY;WKST=MO;BYDAY=TU'), RRule.fromString('FREQ=WEEKLY;WKST=MO;BYDAY=WE')));
     });
     it('handles non intersecting daily byhour', function () {
-        assert.isNull(index_1.intersection(RRule.fromString('FREQ=DAILY;BYHOUR=7,17'), RRule.fromString('FREQ=DAILY;BYHOUR=12,22')));
+        assert.isNull(rrule_ops_1.intersection(RRule.fromString('FREQ=DAILY;BYHOUR=7,17'), RRule.fromString('FREQ=DAILY;BYHOUR=12,22')));
     });
     it('returns a match', function () {
-        let result = index_1.intersection(RRule.fromString('FREQ=DAILY;BYHOUR=7,17'), RRule.fromString('FREQ=DAILY;BYHOUR=7,12,22'), 1);
+        let result = rrule_ops_1.intersection(RRule.fromString('FREQ=DAILY;BYHOUR=7,17'), RRule.fromString('FREQ=DAILY;BYHOUR=7,12,22'), 1);
         assert.typeOf(result, 'Date');
         assert.isAtLeast(result.valueOf(), new Date().valueOf());
     });
     it('returns undefined if no match within specified search time', function () {
-        let result = index_1.intersection(RRule.fromString('FREQ=DAILY;BYHOUR=7,17;BYMINUTE=0;BYSECOND=0'), RRule.fromString('FREQ=DAILY;DTSTART=40990102T000000Z;WKST=MO;BYHOUR=17,7;BYMINUTE=0;BYSECOND=0'), 1);
+        let result = rrule_ops_1.intersection(RRule.fromString('FREQ=DAILY;BYHOUR=7,17;BYMINUTE=0;BYSECOND=0'), RRule.fromString('FREQ=DAILY;DTSTART=40990102T000000Z;WKST=MO;BYHOUR=17,7;BYMINUTE=0;BYSECOND=0'), 1);
         assert.typeOf(result, 'undefined');
+    });
+    it('returns another match', function () {
+        let result = rrule_ops_1.intersection(RRule.fromString('FREQ=DAILY;BYHOUR=7,12,17,22;DTSTART=20171216T125807Z;BYMINUTE=0;BYSECOND=0'), RRule.fromString('FREQ=DAILY;DTSTART=20150101T070000Z;BYHOUR=7,12,17,22'), 1);
+        assert.typeOf(result, 'Date');
+        assert.isAtLeast(result.valueOf(), new Date().valueOf());
     });
 });
--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,7 @@
 import * as RRule from 'rrule-alt';
 import * as chai from "chai";
 
-import {intersection} from '../index'
+import {intersection} from '../rrule-ops'
 
 let assert = chai.assert;
 
@@ -24,6 +24,14 @@ describe('intersections', function() {
   it('returns undefined if no match within specified search time', function() {
     let result = intersection(RRule.fromString('FREQ=DAILY;BYHOUR=7,17;BYMINUTE=0;BYSECOND=0'),RRule.fromString('FREQ=DAILY;DTSTART=40990102T000000Z;WKST=MO;BYHOUR=17,7;BYMINUTE=0;BYSECOND=0'),1);
     assert.typeOf(result, 'undefined');
+  })
+
+  it('returns another match', function() {
+    let result = intersection(
+      RRule.fromString('FREQ=DAILY;BYHOUR=7,12,17,22;DTSTART=20171216T125807Z;BYMINUTE=0;BYSECOND=0'),
+      RRule.fromString('FREQ=DAILY;DTSTART=20150101T070000Z;BYHOUR=7,12,17,22'),1);
+    assert.typeOf(result, 'Date');
+    assert.isAtLeast(result.valueOf(), new Date().valueOf())
   })
 
 });
